9457ebc2824b37f37779cefd377986c1cef52e76
fam007e <fam007e@users.noreply.github.com>
2026-02-06 18:15:34 +0000

    deploy: a2778f3e62e83153e8d07ba100eba76aeb8509c3

--- search/all_0.js
+++ search/all_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['01_2006_0',['[2026.01.06]',['../md_CHANGELOG.html#autotoc_md62',1,'']]],
   ['01_2017_1',['[2026.01.17]',['../md_CHANGELOG.html#autotoc_md69',1,'']]],
-  ['02_2006_2',['[2026.02.06]',['../md_CHANGELOG.html#autotoc_md72',1,'']]],
-  ['06_3',['06',['../md_CHANGELOG.html#autotoc_md62',1,'[2026.01.06]'],['../md_CHANGELOG.html#autotoc_md72',1,'[2026.02.06]']]]
+  ['02_2007_2',['[2026.02.07]',['../md_CHANGELOG.html#autotoc_md72',1,'']]],
+  ['06_3',['[2026.01.06]',['../md_CHANGELOG.html#autotoc_md62',1,'']]],
+  ['07_4',['[2026.02.07]',['../md_CHANGELOG.html#autotoc_md72',1,'']]]
 ];
